2773fdd982ae7dae566a5d96b9a1caebe735a2e5
Cesar Delgado <cesardelgado73@hotmail.com>
2019-03-05 00:48:39 -0400

    Agregado solicitud de Prestamos

---
+++ COOPERATIVAUAPA/Models/EstatusSolicitud.cs
@@ -0,0 +1,10 @@
+﻿namespace COOPERATIVAUAPA.Models
+{
+    public class EstatusSolicitud
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+
+
+    }
+}
---
+++ COOPERATIVAUAPA/Models/TipoPrestamo.cs
@@ -0,0 +1,10 @@
+﻿namespace COOPERATIVAUAPA.Models
+{
+    public class TipoPrestamo
+    {
+        public int Id { get; set; }
+
+        public string Nombre { get; set; }
+
+    }
+}
